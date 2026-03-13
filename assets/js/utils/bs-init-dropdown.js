a30e8db90523de8bc15e4eefac535f754f828142
sebousan <sebousan@gmail.com>
2026-03-13 17:44:57 +0100

    remove all js

--- assets/js/utils/bs-init-dropdown.js
+++
@@ -1,8 +0,0 @@
-import Dropdown from 'js/bootstrap/src/dropdown';
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.js-dropdown-toggle').forEach(trigger => {
-    trigger.setAttribute('data-bs-toggle', 'dropdown');
-    new Dropdown(trigger);
-  });
-});
